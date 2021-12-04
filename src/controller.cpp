102dd66243cfe19d3aae5fe15cb61d1024ccd941
Samuele <sampedra@tiscali.it>
2021-12-04 11:05:09 +0100

    controller updated

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
+#include "Assignment2_RT1/Velocity.h"
 
 //#define SIZE 100
 
@@ -8,54 +9,60 @@ ros::Publisher pub;
 geometry_msgs::Twist robot_vel;
 
 float front_min = 2;
-float front_max = 2;
+float initial_vel = 1;
 
 float checkDistance(float angle_range[], float min_value, float max_value)
 {
-	float value = 50;
-    for (int i = min_value; i<max_value; i++)
+    float value = 50;
+    for (int i = min_value; i < max_value; i++)
     {
-        if(angle_range[i]< value) value = angle_range[i];
+        if (angle_range[i] < value)
+            value = angle_range[i];
     }
     return value;
 }
 
-void ControlRobotTrack(const sensor_msgs::LaserScan::ConstPtr& msg)
+void ControlRobotTrack(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
     float range_view[720];
 
-    float left, right, front; 
+    float left, right, front;
 
-    for (int i=0; i<=720; i++) range_view[i] = msg->ranges[i];
+    for (int i = 0; i <= 720; i++)
+        range_view[i] = msg->ranges[i];
 
     left = checkDistance(range_view, 0, 100);
     right = checkDistance(range_view, 620, 720);
     front = checkDistance(range_view, 310, 410);
 
-
-    if(front<front_min)
+    if (front < front_min)
     {
-        if(left<right)
+        if (left < right)
         {
             robot_vel.linear.x = 0.2;
             robot_vel.angular.y = 1;
         }
-        else if(right<left)
+        else if (right < left)
         {
             robot_vel.linear.x = 0.2;
             robot_vel.angular.y = -1;
         }
     }
-    else if(front>front_min)
+    else if (front > front_min)
     {
-        robot_vel.linear.x = 1;
+        robot_vel.linear.x = initial_vel;
         robot_vel.angular.y = 0;
     }
 
-    ROS_INFO("Right: @[%f]", 0);    
+    ROS_INFO("Right: @[%f]", initial_vel);
     pub.publish(robot_vel);
 }
 
+void ManageSpeed(const Assignment2_RT1::Velocity::ConstPtr& speed)
+{
+    std::cout << "New value of seppd is: "<<speed->velocity_msg<<"\n";
+    float vel = speed->velocity_msg;
+}
 
 int main(int argc, char **argv)
 {
@@ -65,7 +72,8 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     // Define the subscriber to turtle's position
     ros::Subscriber sub = nh.subscribe("/base_scan", 1, ControlRobotTrack);
-    pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);   
+    ros::Subscriber sub2 = nh.subscribe("/Velocity", 1, ManageSpeed);
+    pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
     ros::spin();
     return 0;
 }
